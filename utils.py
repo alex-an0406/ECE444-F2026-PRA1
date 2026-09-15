1dc509491dad8a06bfbdc517c471a55ec19bb5d9
Alex <alexan0406@gmail.com>
2026-09-15 10:05:31 -0400

    added comments

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 class utils:
+    #returns the reversed integer of the input
     def reversed(self, number: int) -> int:
         if not isinstance(number, int):
             raise TypeError("Input must be an integer")
         return int(str(number)[::-1])
-
+#returns int as binary and octal representation of the input
     def formatter(self, number: int):
         if not isinstance(number, int):
             raise TypeError("Input must be an integer")
